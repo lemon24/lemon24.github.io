d95e379b2f926a5360203275237536c550ce6427
lemon24 <damian.adrian24@gmail.com>
2025-04-11 10:40:10 +0300

    deploy

---
+++ _file/ptpe/bench.py
@@ -0,0 +1,166 @@
+import time
+import threading
+from contextlib import contextmanager
+
+try:
+    import psutil
+except ImportError:
+    pass
+
+
+class Client:
+    io_time = 0.02
+    cpu_time = 0.0008
+
+    def method(self, arg):
+        # simulate I/O
+        time.sleep(self.io_time)
+
+        # simulate CPU-bound work
+        start = time.perf_counter()
+        while time.perf_counter() - start < self.cpu_time:
+            for i in range(100): i ** i
+
+        return arg
+
+
+client = None
+
+def init_client():
+    global client
+    client = Client()
+
+def do_stuff(*args):
+    return client.method(*args)
+
+
+@contextmanager
+def timer():
+    start = time.perf_counter()
+    yield
+    end = time.perf_counter()
+    print(f"elapsed: {end-start:1.3f}")
+
+
+def benchmark(executor, n=10_000, timer=timer):
+    with executor:
+        # make sure all the workers are started,
+        # so we don't measure their startup time
+        list(executor.map(time.sleep, [0] * 200))
+
+        with timer():
+            values = list(executor.map(do_stuff, range(n), chunksize=10))
+
+        assert values == list(range(n)), values
+
+
+@contextmanager
+def top():
+    """Print information about current and child processes.
+
+    RES is the resident set size. USS is the unique set size.
+    %CPU is the CPU utilization. nTH is the number of threads.
+
+    """
+    process = psutil.Process()
+    processes = [process] + process.children(True)
+    for p in processes: p.cpu_percent()
+
+    yield
+
+    print(f"{'PID':>7} {'RES':>7} {'USS':>7} {'%CPU':>7} {'nTH':>7}")
+    for p in processes:
+        try:
+            m = p.memory_full_info()
+        except psutil.AccessDenied:
+            m = p.memory_info()
+        rss = m.rss / 2**20
+        uss = getattr(m, 'uss', 0) / 2**20
+        cpu = p.cpu_percent()
+        nth = p.num_threads()
+        print(f"{p.pid:>7} {rss:6.1f}m {uss:6.1f}m {cpu:7.1f} {nth:>7}")
+
+
+@contextmanager
+def terminate_children(interval=1):
+    def terminate():
+        for child in psutil.Process().children():
+            child.terminate()
+    threading.Timer(interval, terminate).start()
+    yield
+
+
+from ptpe import ProcessThreadPoolExecutor
+
+
+def debug_shutdown(cls=ProcessThreadPoolExecutor):
+    """ python -c 'from bench import *; debug_shutdown()' """
+    with cls(2) as executor:
+        executor.submit(_sleep, 2)
+        time.sleep(1)
+        print('before shut down')
+        executor.shutdown()
+        print('after shut down')
+    print('after with')
+
+def _sleep(n):
+    print('sleeping', n)
+    time.sleep(n)
+    print('slept', n)
+
+
+def debug_kill(kill_interval=1, print_interval=.5, cls=ProcessThreadPoolExecutor):
+    """ python -c 'from bench import *; debug_kill(1) """
+    print('starting')
+    executor = cls(2, initializer=init_client)
+
+    def print_debug():
+        print(
+            f"len(tasks)={len(executor._ProcessThreadPoolExecutor__tasks)} "
+            f"PPE.broken={executor._broken} "
+        )
+        if executor._broken:
+            return
+        threading.Timer(print_interval, print_debug).start()
+
+    print_debug()
+    benchmark(executor, timer=lambda: terminate_children(kill_interval))
+    print('done')
+
+
+lines = """\
+# benchmark(ProcessThreadPoolExecutor(10, initializer=init_client))
+# benchmark(ProcessThreadPoolExecutor(20, initializer=init_client))
+# benchmark(ProcessThreadPoolExecutor(30, initializer=init_client))
+benchmark(ProcessThreadPoolExecutor(40, initializer=init_client))
+
+init_client()
+# benchmark(ThreadPoolExecutor(10))
+# benchmark(ThreadPoolExecutor(20))
+# benchmark(ThreadPoolExecutor(30))
+benchmark(ThreadPoolExecutor(40))
+
+# benchmark(ProcessPoolExecutor(10, initializer=init_client))
+# benchmark(ProcessPoolExecutor(20, initializer=init_client))
+# benchmark(ProcessPoolExecutor(30, initializer=init_client))
+benchmark(ProcessPoolExecutor(40, initializer=init_client))
+
+""".splitlines()
+
+
+if __name__ == '__main__':
+    import sys
+    from concurrent.futures import *
+
+    def repl_eval(expr):
+        expr = expr.strip()
+        if not expr or expr.startswith('#'):
+            return
+        print(f">>> {expr}")
+        rv = eval(expr)
+        if rv is not None:
+            print(rv)
+
+    if not sys.flags.interactive:
+        for line in lines:
+            repl_eval(line)
---
+++ _file/ptpe/ptpe.py
@@ -0,0 +1,113 @@
+import queue
+import atexit
+import threading
+import multiprocessing
+import concurrent.futures
+
+
+class ProcessThreadPoolExecutor(concurrent.futures.ProcessPoolExecutor):
+
+    def __init__(self, max_threads=None, initializer=None, initargs=()):
+        self.__result_queue = multiprocessing.Queue()
+        super().__init__(
+            initializer=_init_process,
+            initargs=(self.__result_queue, max_threads, initializer, initargs)
+        )
+        self.__tasks = {}
+        self.__result_handler = threading.Thread(target=self.__handle_results)
+        self.__result_handler.start()
+
+    def submit(self, fn, *args, **kwargs):
+        outer = concurrent.futures.Future()
+        task_id = id(outer)
+        self.__tasks[task_id] = outer
+
+        outer.set_running_or_notify_cancel()
+        inner = super().submit(_submit, task_id, fn, *args, **kwargs)
+        inner.task_id = task_id
+        inner.add_done_callback(self.__handle_inner)
+
+        return outer
+
+    def __handle_results(self):
+        while True:
+            try:
+                value = self.__result_queue.get(timeout=.1)
+            except queue.Empty:
+                try:
+                    super().submit(int).cancel()
+                except concurrent.futures.BrokenExecutor as e:
+                    exc = type(e)(str(e))
+                    break
+                except RuntimeError as e:
+                    assert 'shutdown' in str(e), e
+                continue
+
+            if not value:
+                return
+
+            task_id, ok, result = value
+            if outer := self.__tasks.pop(task_id, None):
+                if ok:
+                    outer.set_result(result)
+                else:
+                    outer.set_exception(result)
+
+        while self.__tasks:
+            try:
+                _, outer = self.__tasks.popitem()
+            except KeyError:
+                break
+            outer.set_exception(exc)
+
+    def __handle_inner(self, inner):
+        task_id = inner.task_id
+        if exception := inner.exception():
+            if outer := self.__tasks.pop(task_id, None):
+                outer.set_exception(exception)
+
+    def shutdown(self, wait=True):
+        super().shutdown(wait=wait)
+        if self.__result_queue:
+            self.__result_queue.put(None)
+            if wait:
+                self.__result_handler.join()
+            self.__result_queue.close()
+            self.__result_queue = None
+
+
+_executor = None
+_result_queue = None
+
+def _init_process(queue, max_threads, initializer, initargs):
+    global _executor, _result_queue
+
+    _executor = concurrent.futures.ThreadPoolExecutor(max_threads)
+    atexit.register(_executor.shutdown)
+
+    _result_queue = queue
+    atexit.register(_result_queue.close)
+
+    if initializer:
+        initializer(*initargs)
+
+
+def _submit(task_id, fn, *args, **kwargs):
+    task = _executor.submit(fn, *args, **kwargs)
+    task.task_id = task_id
+    task.add_done_callback(_put_result)
+
+def _put_result(task):
+    if exception := task.exception():
+        _result_queue.put((task.task_id, False, exception))
+    else:
+        _result_queue.put((task.task_id, True, task.result()))
+
+
+def _do_stuff(n):
+    print(f"doing: {n}")
+    return n ** 2
+
+if __name__ == '__main__':
+    with ProcessThreadPoolExecutor() as e:
+        print(list(e.map(_do_stuff, [0, 1, 2])))
